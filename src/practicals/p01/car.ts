24766ffe4bc6ab08fcbc91d64b45b1bf620969fd
Lukazx15 <nattananpunpetch@gmail.com>
2026-02-20 17:42:43 +0700

    test01

--- src/practicals/p01/car.ts
+++ src/practicals/p01/car.ts
@@ -1,1 +1,19 @@
-export class Car {}
+export class Car {
+    public brand: string = "";
+    public model: string = "";
+
+    constructor
+
+    public start(): void;{
+        console.log(`${this.brand} ${this.model} is starting...`);
+    }
+    public brake(): void {
+        console.log(`${this.brand} ${this.model} is braking...`);
+    }
+    public stop(): void {
+        console.log(`${this.brand} ${this.model} is stopping...`);
+    }
+
+
+
+}
