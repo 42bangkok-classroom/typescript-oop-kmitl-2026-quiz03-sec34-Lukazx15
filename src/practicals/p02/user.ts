f33aa95a44dfe99e73c1f46852789fa19a7fbf3c
Lukazx15 <nattananpunpetch@gmail.com>
2026-02-20 18:07:49 +0700

    test04

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,22 @@
-export class User {}
+export class User {
+    public firstname: string;
+    public lastname: string;
+    private age: number;
+
+    static BIRTH_YEAR: number = new Date().getFullYear();
+
+    constructor(){
+        this.firstname = "";
+        this.lastname = "";
+        this.age = 0;
+
+    }
+    setAge(age: number): void {
+        if (age <= 0){
+            this.age = age;
+        }
+    }
+
+
+}
+    
