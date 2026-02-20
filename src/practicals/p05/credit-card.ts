73ddc4a65f04ed674f37eabe675ceeb2a27cab06
Lukazx15 <nattananpunpetch@gmail.com>
2026-02-20 18:55:19 +0700

    test11

--- src/practicals/p05/credit-card.ts
+++ src/practicals/p05/credit-card.ts
@@ -1,1 +1,19 @@
-export class CreditCard {}
+import { Payable } from "./payable";
+import { PaymentMethod } from "./payment-method";
+
+export class CreditCard extends PaymentMethod implements Payable {
+  private cardNumber: string;
+
+constructor(accountName: string, cardNumber: string) {
+    super(accountName);
+    this.cardNumber = cardNumber;
+}
+
+public processPayment(amount: number): boolean {
+    console.log(`Cutting credit card balance: ${amount} Baht`);
+    return true;
+}
+public getPaymentFee(amount: number): number {
+    return amount * 0.02;
+  }
+}
--- src/practicals/p05/ewallet.ts
+++ src/practicals/p05/ewallet.ts
@@ -1,1 +1,22 @@
-export class EWallet {}
+import { Payable } from "./payable";
+import { PaymentMethod } from "./payment-method";
+
+export class EWallet extends PaymentMethod implements Payable {
+    private walletId: string;
+
+constructor(accountName: string, walletId: string) {
+    super(accountName);
+    this.walletId = walletId;
+}
+
+public getAccountName(): string {
+    return this.accountName;
+}
+public processPayment(amount: number): boolean {
+    console.log(`Cutting e-wallet balance: ${amount} Baht`);
+    return true;
+}
+public getPaymentFee(amount: number): number {
+    return amount * 0.01;
+ }
+}
