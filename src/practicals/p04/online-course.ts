052a67b5e89aacc45783dce64ddc7b875d8a461e
Lukazx15 <nattananpunpetch@gmail.com>
2026-02-20 18:42:10 +0700

    test10

--- src/practicals/p04/online-course.ts
+++ src/practicals/p04/online-course.ts
@@ -1,1 +1,38 @@
-export class OnlineCourse {}
+export class OnlineCourse {
+  public courseName: string;
+  public maxStudents: number;
+
+  private enrolledStudents: number;
+  private isOpen: boolean;
+
+  constructor(courseName: string, maxStudents: number) {
+    this.courseName = courseName;
+    this.maxStudents = maxStudents;
+    this.enrolledStudents = 0;
+    this.isOpen = true;
+  }
+
+  enroll(): boolean {
+    if (this.canEnroll()) {
+      this.enrolledStudents++;
+      return true;
+    }
+    return false;
+  }
+
+  closeCourse(): void {
+    this.isOpen = false;
+  }
+
+  getAvailableSeats(): number {
+    return this.maxStudents - this.enrolledStudents;
+  }
+
+  getCourseStatus(): string {
+    return this.isOpen ? "Open" : "Closed";
+  }
+
+  private canEnroll(): boolean {
+    return this.isOpen && this.enrolledStudents < this.maxStudents;
+  }
+}
